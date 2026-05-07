14b0fec1780571be48dbc03f5737b6b74d6ca65e
dyphire <qimoge@gmail.com>
2026-05-08 05:08:20 +0800

    drop expat

--- packages/fontconfig.cmake
+++ packages/fontconfig.cmake
@@ -19,6 +19,7 @@ ExternalProject_Add(fontconfig
         --cross-file=${MESON_CROSS}
         --buildtype=release
         --default-library=static
+        -Dxml-backend=libxml2
         -Ddoc=disabled
         -Dtests=disabled
         -Dtools=disabled
